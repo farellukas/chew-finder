825aa46e0051228f0730813e16a5f6b4a337635e
Farel Lukas <farellukas@gmail.com>
2022-10-15 20:40:25 -0600

    Created API endpoint

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const yelp = require('yelp-fusion');
+
+const apiKey = process.env.API_KEY;
+
+const app = express();
+const port = 3000;
+const client = yelp.client(apiKey)
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {console.log(`listening on port ${port}`)});
+
+app.get('/api/search', (req, res) => {
+    const term = req.query.term;
+    const location = req.query.location;
+    
+    const searchRequest = {
+        term,
+        location
+    };
+
+    client.search(searchRequest).then(response => {
+        const body = response.jsonBody;
+        res.send(body);
+    }).catch(err => {
+        console.log(err);
+        res.send(err.message);
+    });  
+})
