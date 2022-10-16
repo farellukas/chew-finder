16b724d5790bbd019d9d2beb08fb8441afc94213
Farel Lukas <farellukas@gmail.com>
2022-10-16 01:30:43 -0600

    Added yelp logo

--- frontend/src/containers/Hero.tsx
+++ frontend/src/containers/Hero.tsx
@@ -20,7 +20,7 @@ function Hero({ coords, setCoords }:Props) {
   return (
     <div className="flex h-screen">
       <div className="m-auto">
-        <h1 className="font-sans font-bold text-center text-6xl pb-10">[NAME OF APP]</h1>
+        <h1 className="font-sans font-bold text-center text-6xl pb-10">CHEW FINDER</h1>
         <Button text="DISCOVER RESTAURANTS NOW" linkto="/discover" primary={true} handleClick={handleClick} />
       </div>
     </div>
