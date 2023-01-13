bd499afb0f60570a8b8503b83c6209b725c22144
Farel Lukas <farellukas@gmail.com>
2023-01-13 12:28:53 -0700

    Replaced backend with deployed backend

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,46 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const yelp = require('yelp-fusion');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const yelp = require("yelp-fusion");
 
 const apiKey = process.env.API_KEY;
 
 const app = express();
-const port = 5000;
-const client = yelp.client(apiKey)
+const port = process.env.PORT || 3000;
+const client = yelp.client(apiKey);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.listen(port, () => {console.log(`listening on port ${port}`)});
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
 
-app.get('/api/search', (req, res) => {
-    const term = req.query.term;
-    const latitude = req.query.latitude;
-    const longitude = req.query.longitude;
-    const offset = req.query.offset;
-    
-    const searchRequest = {
-        term,
-        latitude,
-        longitude,
-        offset
-    };
+app.get("/api/search", (req, res) => {
+  const term = req.query.term;
+  const latitude = req.query.latitude;
+  const longitude = req.query.longitude;
+  const offset = req.query.offset;
 
-    client.search(searchRequest).then(response => {
-        const body = response.jsonBody;
-        res.send(body);
-    }).catch(err => {
-        console.log(err);
-        res.send(err.message);
-    });  
-})
+  const searchRequest = {
+    term,
+    latitude,
+    longitude,
+    offset,
+  };
+
+  client
+    .search(searchRequest)
+    .then((response) => {
+      const body = response.jsonBody;
+      res.send(body);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(err.message);
+    });
+});
